6252fb5cb50fb6131d06f93d9d39df7637a7f47a
malucodenoia <57994787+malucodenoia@users.noreply.github.com>
2019-11-20 10:26:47 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# crossover
+Run The Script For Installing Crossover.
+After running it Create Many Bottles You Needed.
+And Uncomment/remove The "#" after you created those bottles.
+and run the script again.
+
+MY Youtube Channel : 
+https://www.youtube.com/channel/UClCOKiqxfjgH-17mq64sQLw
---
+++ c.sh
@@ -0,0 +1,14 @@
+echo " Installing Crossover Crack Linux Full Version"
+sudo dpkg --add-architecture i386 ; sudo apt-get update
+sudo sh -c "apt-get update; apt-get install libnss-mdns:i386"
+wget http://download2044.mediafireuserdownload.com/b0zmzzfs9urg/mbg3ct7fdgzjf5b/crossover16.bin
+sudo sh install-crossover-16.0.0.bin
+
+
+#If You Like The Program Please Support Them @ https://www.crossover.com/ Buy It.
+#Create some bootles you will use. (after crack u will be not able to create more bootles!!!)
+
+#Uncomment The "#" Following Lines Under If You Already Created Bottles You Needed. 
+
+#wget http://download1585.mediafireuserdownload.com/vfx9bjxb2ycg/yb78ajlxhqzsj46/winewrapper.exe.so
+#sudo cp winewrapper.exe.so /opt/cxoffice/lib/wine
